a00fc185daf5d589ef6ee406f8a90911f98dc7bb
Aaron Grincewicz <asgrincewicz@icloud.com>
2021-10-25 13:05:40 -0400

    Project Restructure

---
+++ Assets/CardGamePrototype/Scripts/Card Components/CardComponent.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public abstract class CardComponent : MonoBehaviour
+{
+    [SerializeField] protected AssetReference _cardData;
+    [SerializeField] protected Image _baseImage = null;
+    [SerializeField] protected Image _artImage = null;
+    [SerializeField] protected Image _statsImage = null;
+    [SerializeField] protected TMP_Text _titleText = null;
+    [SerializeField] protected TMP_Text _gameplayText = null;
+    
+    
+    protected abstract void OnEnable();
+    protected abstract void OnDisable();
+    /// <summary>
+    /// This method is required to populate the info on the card game object.
+    /// </summary>
+    protected abstract void PopulateCardInfo();
+}
